8d54bc49d990c3fe64adc19ed262dedb3c17558b
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-02 15:43:09 +0100

    Creato form comics.edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,47 +1,52 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <section id='new-comic' class="form">
+    <section id='edit-comic' class="form">
         <div class="container">
-            <h1>Aggiungi Fumetto</h1>
-            <form action="{{ route('comics.store') }}" method="POST">
+            <h1>Modifica Fumetto</h1>
+            <form action="{{ route('comics.update', $comic->id) }}" method="POST">
+                @method('PUT')
                 @csrf
                 <div class="row">
                     <div class="col-4">
                         <label for="title">Titolo</label>
-                        <input type="text" id="title" name="title" placeholder="Inserisci titolo" required>
+                        <input type="text" id="title" name="title" placeholder="Inserisci titolo" required
+                            value="{{ $comic->title }}">
                     </div>
                     <div class="col-4">
                         <label for="thumb">Inserisci immagine copertina</label>
-                        <input type="url" id="thumb" name="thumb" required>
+                        <input type="url" id="thumb" name="thumb" required value="{{ $comic->thumb }}">
                     </div>
                     <div class="col-4">
                         <label for="type">Inserisci il tipo di fumetto</label>
-                        <input type="text" id="type" name="type" placeholder="comic book" required>
+                        <input type="text" id="type" name="type" placeholder="comic book" required
+                            value="{{ $comic->type }}">
                     </div>
                     <div class="col-12">
                         <label for="description">Descrizione</label>
-                        <textarea name="description" id="description" rows="5" required></textarea>
+                        <textarea name="description" id="description" rows="5" required>{{ $comic->description }}</textarea>
                     </div>
                     <div class="col-4">
                         <label for="price">Inserisci prezzo</label>
-                        <input type="text" id="price" name="price" placeholder="$..." required>
+                        <input type="text" id="price" name="price" placeholder="$..." required
+                            value="{{ $comic->price }}">
                     </div>
                     <div class="col-4">
                         <label for="series">Inserisci serie</label>
-                        <input type="text" id="series" name="series" placeholder="Action Comics" required>
+                        <input type="text" id="series" name="series" placeholder="Action Comics" required
+                            value="{{ $comic->series }}">
                     </div>
                     <div class="col-4">
                         <label for="sale_date">Inserisci data di uscita</label>
-                        <input type="date" id="sale_date" name="sale_date" required>
+                        <input type="date" id="sale_date" name="sale_date" required value="{{ $comic->sale_date }}">
                     </div>
                     <div class="col-6">
                         <label for="artists">Inserisci gli artisti</label>
-                        <textarea name="artists" id="artists" rows="5" required></textarea>
+                        <textarea name="artists" id="artists" rows="5" required>{{ implode(', ', $comic->artists) }}</textarea>
                     </div>
                     <div class="col-6">
                         <label for="writers">Inserisci gli scrittori</label>
-                        <textarea name="writers" id="writers" rows="5" required></textarea>
+                        <textarea name="writers" id="writers" rows="5" required>{{ implode(', ', $comic->writers) }}</textarea>
                     </div>
                     <button type="submit">Salva</button>
                 </div>
